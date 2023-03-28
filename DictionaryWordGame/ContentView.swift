45275b648322c580b35e1b7d52b8ce48c297d560
Fiona Kate <fkmorgan@me.com>
2023-03-28 15:56:04 +0100

    final version

--- DictionaryWordGame/ContentView.swift
+++ DictionaryWordGame/ContentView.swift
@@ -8,19 +8,102 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inputText = ""
+    @State var guessedWords: [String] = []
+    @State var validWords: [String] = []
+    @State var correctWord = ""
+    @State var gameEnded = false
+    @FocusState var wordInFocus: Bool
+    @State var counter = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("Please enter your word", text: $inputText)
+                .focused($wordInFocus)
+                .onSubmit {
+                    checkWord()
+                }
+                .padding()
+            
+            List {
+                ForEach(guessedWords.sorted(), id: \.self) { word in
+                    if word == correctWord {
+                        if gameEnded {
+                            Text(word)
+                                .foregroundColor(.green)
+                        } else {
+                            Text("???")
+                        }
+                    } else {
+                        Text(word)
+                    }
+                }
+            }
+            .onAppear() {
+                validWords = loadWords(for: "valid-words")
+                loadCorrectWord()
+            }
+            .alert(isPresented: $gameEnded) {
+                Alert(title: Text("Well done"),
+                      message: Text("You guessed the word was \"\(correctWord)\""),
+                      primaryButton: .default(Text("New Game")) {
+                    resetGame()
+                },
+                      secondaryButton: .cancel())
+                
+            }
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    func checkWord() {
+        // is it correct?
+        if inputText.lowercased() == correctWord {
+            
+            counter += 1
+            print("well done")
+            gameEnded = true
+            
+        } else if validWords.contains(inputText.lowercased()) {
+            counter += 1
+            
+            // append it to guessed words array
+            guessedWords.append(inputText.lowercased())
+            // reset input text
+            inputText = ""
+            wordInFocus = true
+        }
+    }
+    
+    func loadWords(for filename: String) -> [String] {
+        var words: [String] = []
+        
+        if let wordsURL = Bundle.main.url(forResource: filename, withExtension: "json") {
+            
+            if let data = try? Data(contentsOf: wordsURL) {
+                
+                if let json = parseStrings(json: data) {
+                    
+                    for item in json {
+                        if let word = item.1.dictionaryValue.first?.value.rawValue {
+                            words.append("\(word)")
+                        }
+                    }
+                }
+            }
+        }
+        return words
+    }
+    
+    func loadCorrectWord() {
+        correctWord = loadWords(for: "correct-words").randomElement() ?? "house"
+        print("correct word: \(correctWord)")
+        guessedWords.append(correctWord)
+    }
+    
+    func resetGame() {
+        guessedWords = []
+        loadCorrectWord()
+        inputText = ""
+        counter = 0
     }
 }
