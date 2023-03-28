9591d2c56e81cbc1e0ee01aa500a7156e3537dd3
Fiona Kate <fkmorgan@me.com>
2023-03-28 15:05:14 +0100

    Initial Commit

---
+++ DictionaryWordGame/DictionaryWordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryWordGameApp.swift
+//  DictionaryWordGame
+//
+//  Created by Fiona Kate Morgan on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DictionaryWordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
